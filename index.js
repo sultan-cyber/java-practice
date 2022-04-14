28d7f8e8f3ea7150d6bd7812bc874edd60ab568e
sultan-cyber <isadique0@gmail.com>
2022-04-14 11:21:43 +0100

    java commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var name = "Adewale";
+console.log(name)
+var x; y;
+var x =12;
+var y = 9;
+var z;
+var z = x+y;
+console.log(z)
+var School; church;
+var School ="Fuoye"
+var church = "Redeem"
+var bestChoice =School + " " +church;
+console.log(bestChoice)
+let people = "BlackPeople"
+let language= "ÿorubaLang"
+
+
+
+let first_number = 20
+let second_number = 40
+let sum = first_number + second_number
+let subtract = second_number - first_number
+let divide = second_number/first_number
+let multiply = second_number * first_number
+console.log(sum)
+console.log(subtract)
+console.log(divide)
+console.log(multiply)
