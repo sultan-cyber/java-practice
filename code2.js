d0b880ae7d4cb2f3eabc097dd797c2a0a5d79e00
sultan-cyber <isadique0@gmail.com>
2022-04-20 11:34:59 +0100

    jdkj

---
+++ code2.js
@@ -0,0 +1,57 @@
+// basically some strings methods include:
+// 1. lenght
+// 2. slice
+// 3. substring
+// 4. subtract
+// 5. locate
+// 6. upper and lower class
+// 7. replace
+// 8. concatenation
+// 9. mutation
+
+// var myName = "Sultan"
+// var catchment_phrase = "No be today yansh, don day back. E don tey wey we dey shayo."
+// var Occupation = "TEACHER"
+// let trans = myName.toUpperCase()
+// let trans2 = Occupation.toLowerCase()
+// console.log(trans2)
+// console.log(trans)
+
+// const howMany = myName.length
+// console.log(howMany)
+
+// var slit = catchment_phrase.slice(0, 18)
+// var slitt = catchment_phrase.slice(19, -10)
+// let sliit = catchment_phrase.substring(32, catchment_phrase.length)
+// const sat = catchment_phrase.substr(0, 35)
+// let repla = catchment_phrase.replace("yansh", "pocket")
+// let repo = repla.replace(/SHAYO/i, "enjoy")
+// console.log(slit)
+// console.log(slitt)
+// console.log(sliit)
+// console.log(sat)
+// console.log(repla)
+// console.log(repo)
+
+// // // mutation
+// let cars = ["volvo","benz","volkswagen","golf","Audi","Bmw"]
+// cars[2] ="danfo"
+// console.log(cars[2])
+// console.log(cars)
+// var myName = "Sultan"
+// console.log(myName[4])
+// myName[4] = "o"
+// console.log(myName)
+
+// let trimmable ="Ahme d Bolarinwa  b   ";
+// console.log(trimmable.trim())
+// let text = "5"
+// let padded = text.padEnd(4,"x")
+// console.log(padded)
+
+// string searching
+// let str3 = "Please locate where 'locate' occurs!";
+// console.log(str3.indexOf("locate"));
+// console.log(str3.search("'locate'"));
+// console.log(str3.includes("locate"));
+// console.log(str3.startsWith("Please locate"));
--- index.js
+++ index.js
@@ -1,28 +1,160 @@
-var name = "Adewale";
-console.log(name)
-var x; y;
-var x =12;
-var y = 9;
-var z;
-var z = x+y;
-console.log(z)
-var School; church;
-var School ="Fuoye"
-var church = "Redeem"
-var bestChoice =School + " " +church;
-console.log(bestChoice)
-let people = "BlackPeople"
-let language= "ÿorubaLang"
+// day 1(wednesday 13th of April class taken by Ahmed Bolarinwa)
+// var name = "Adewale";
+// console.log(name)
+// var x; y;
+// var x =12;
+// var y = 9;
+// var z;
+// var z = x+y;
+// console.log(z)
+// var School; church;
+// var School ="Fuoye"
+// var church = "Redeem"
+// var bestChoice =School + " " +church;
+// console.log(bestChoice)
+// let people = "BlackPeople"
+// let language= "ÿorubaLang"
+
+// let first_number = 20
+// let second_number = 40
+// let sum = first_number + second_number
+// let subtract = second_number - first_number
+// let divide = second_number/first_number
+// let multiply = second_number * first_number
+// console.log(sum)
+// console.log(subtract)
+// console.log(divide)
+// console.log(multiply)
+
+// Day 2 classwork( 14th April 2022 class by Ahmed Bolarinwa on data types)
+// Number
+// String
+// Boolean
+// Array(Object)
+// Object
+// null
+// undefined
+
+// let age = 18
+// let firstName = "Adewale"
+// let IsMuslim = true
+// let BestCar = ["ferrari", "Bugati", "Rolce Royce"]
+// let LifeStyle = {BestFood: "Beans and Plantain", current_job: "Teaching", Hobby:"Singing",}
+// let jk = null
+// let pc;
+
+// console.log(age)
+// console.log(firstName)
+// console.log(IsMuslim)
+// console.log(BestCar)
+// console.log(LifeStyle)
+// console.log(jk)
+// console.log(pc)
+
+// console.log(typeof(age))
+// console.log(typeof(firstName))
+// console.log(typeof(IsMuslim))
+// console.log(typeof(BestCar))
+// console.log(typeof(LifeStyle))
+// console.log(typeof(jk))
+// console.log(typeof(pc))
+
+//  age = undefined
+//  firstName = undefined
+// IsMuslim = undefined
+//  BestCar = undefined
+//  LifeStyle = undefined
+//  jk = undefined
+//  pc;
+
+//  console.log(typeof(age))
+//  console.log(typeof(firstName))
+//  console.log(typeof(IsMuslim))
+//  console.log(typeof(BestCar))
+//  console.log(typeof(LifeStyle))
+//  console.log(typeof(jk))
+//  console.log(typeof(pc))
+ 
 
 
 
-let first_number = 20
-let second_number = 40
-let sum = first_number + second_number
-let subtract = second_number - first_number
-let divide = second_number/first_number
-let multiply = second_number * first_number
-console.log(sum)
-console.log(subtract)
-console.log(divide)
-console.log(multiply)
+/* the various data types in javascript are as follows:
+// Number
+// String
+// Boolean
+// Array(Object)
+// Object
+// null
+// undefined
+
+the NUMBER comprises of only numeric values such as 0, 1, 2, 3, .....
+the STRING values comprise mainly of words 
+they are identified or symbolised with the use of quotes around them,
+ any word written without quotes around them
+will appear to be unexpected.
+The boolean values are basically
+ true or false responses to stated questions.
+ Array comprise of a group of data collected in an single profile
+ Object comprise of data that are assisgned to specfic sub-headings
+null basically means nothing in  the javascript
+undefined are variables that are not assisgned to any variable*/
+
+// Day 3 class on Strings method (April 19th, 2022)
+// basically some strings methods include:
+// 1. lenght
+// 2. slice
+// 3. substring
+// 4. subtract
+// 5. locate
+// 6. upper and lower class
+// 7. replace
+// 8. concatenation
+// 9. mutation
+
+// var myName = "Sultan"
+// var catchment_phrase = "No be today yansh, don day back. E don tey wey we dey shayo."
+// var Occupation = "TEACHER"
+// let trans = myName.toUpperCase()
+// let trans2 = Occupation.toLowerCase()
+// console.log(trans2)
+// console.log(trans)
+
+// const howMany = myName.length
+// console.log(howMany)
+
+// var slit = catchment_phrase.slice(0, 18)
+// var slitt = catchment_phrase.slice(19, -10)
+// let sliit = catchment_phrase.substring(32, catchment_phrase.length)
+// const sat = catchment_phrase.substr(0, 35)
+// let repla = catchment_phrase.replace("yansh", "pocket")
+// let repo = repla.replace(/SHAYO/i, "enjoy")
+// console.log(slit)
+// console.log(slitt)
+// console.log(sliit)
+// console.log(sat)
+// console.log(repla)
+// console.log(repo)
+
+// // // mutation
+// let cars = ["volvo","benz","volkswagen","golf","Audi","Bmw"]
+// cars[2] ="danfo"
+// console.log(cars[2])
+// console.log(cars)
+// var myName = "Sultan"
+// console.log(myName[4])
+// myName[4] = "o"
+// console.log(myName)
+
+// let trimmable ="Ahme d Bolarinwa  b   ";
+// console.log(trimmable.trim())
+// let text = "5"
+// let padded = text.padEnd(4,"x")
+// console.log(padded)
+
+// string searching
+// let str3 = "Please locate where 'locate' occurs!";
+// console.log(str3.indexOf("locate"));
+// console.log(str3.search("'locate'"));
+// console.log(str3.includes("locate"));
+// console.log(str3.startsWith("Please locate"));
+
